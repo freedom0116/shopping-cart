ad2a9671aa0c2f8862c2aadda75efbcfa5e3547c
freedom0116 <jimmy29257415@gmail.com>
2021-03-15 10:43:44 +0800

    next client system

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,16 +2,30 @@ import './Header.css';
 import '../../App.css';
 import { ShoppingCartOutlined } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-function header() {
+function Header() {
+    const cartItems = useSelector(state => state.cartItems);
+
+    // const totalItems = () => {
+    //     let num = 0;
+    //     cartItems.foreach(e => {
+    //         totalItems
+    //     })
+    // }
+
     return (
         <div className="header">
             <Link to="/" className="link-font">Shopping Cart</Link>
-            <Link to="/cart" className="link-font">
+            <Link to="/cart" className="link-font cartIcon">
                 <ShoppingCartOutlined fontSize="large" />
+                {cartItems.length === 0 ?
+                    <div></div> :
+                    <div>{cartItems.length}</div>
+                }
             </Link>
         </div>
     );
 }
 
-export default header;
+export default Header;
